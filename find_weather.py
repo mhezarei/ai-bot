9a716e6aa4b4ecda438afe57c9a41af740baf9b5
matin <zivdar1matin@gmail.com>
2021-02-07 02:27:52 +0330

    Enhance find_weather key_words

--- find_weather.py
+++ find_weather.py
@@ -1,5 +1,5 @@
 def find_weather_method(sentence_lem):
-    temp_key_words = ['دما', 'درجه']
+    temp_key_words = ['دما', 'درجه', 'اندازه', 'چقدر']
     for word in temp_key_words:
         if word in sentence_lem:
             return "temp"
